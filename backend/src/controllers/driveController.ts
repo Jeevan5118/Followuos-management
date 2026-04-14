978889f789c50528ddb9190bb9389a28ab1d6aa6
Jeevan Konduru <jeevankonduru2002@gmail.com>
2026-04-14 18:14:43 +0530

    fix: use shared prisma instance in driveController to support Neon adapter

--- backend/src/controllers/driveController.ts
+++ backend/src/controllers/driveController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../prismaClient';
 
-const prisma = new PrismaClient();
 
 export const createDrive = async (req: Request, res: Response): Promise<void> => {
     try {
